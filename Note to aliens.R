09e47bacc1aea8e3c0d3f45fae3edff6f6958604
William Shancey <williamshancey@Williams-MBP-3.wv.cc.cmu.edu>
2017-12-10 18:11:29 -0500

    commit!

---
+++ Note to aliens.R
@@ -0,0 +1,8 @@
+cat("Hello, Other World!
+
+    If you’re reading this that means that you have discovered evidence of life on planet Earth (and that you can read English somehow). I’d like you to know that there is intelligent life on Earth and that we come in peace (usually). Life here is pretty strange, we humans tend to mess pretty much everything up and aren’t likely to add any kind of benefit to any society you’ve set up on your home planet. In my opinion it would be fruitless to visit Earth to try to help, we’re pretty much a lost cause and it is extremely possible that we blow ourselves up by the time you get here.
+    
+    But rest assured that we are here and we are currently alive and well!!
+    
+    Best regards,
+    Billy Shancey")
